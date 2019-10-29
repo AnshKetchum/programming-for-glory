e65a0a4b99caa3c41de2809c00eb3616eb8a103e
Ansh Chaurasia <achaurasia@Anshs-MacBook-Air.local>
2019-10-29 11:01:24 -0700

    Modified Chocolate Eating Problem

--- Alphastar/Gold_Advanced-2019FallClass/Greedy Methods/Chocolate Eating/eating.cpp
+++ Alphastar/Gold_Advanced-2019FallClass/Greedy Methods/Chocolate Eating/eating.cpp
@@ -6,32 +6,34 @@ using namespace std;
 int n,d,chocolate[MAXN];
 bool pos(int happiness_value)
 {
-    int day_count = 1,sum = 0;
-    for(int i = 0; i < n; i++)
+    int sum = 0;
+    for(int i = 0,day_count = 1; i < n && day_count <= d; i++)
     {
         if(sum < happiness_value)
             sum += chocolate[i];
-
-        while(sum >= happiness_value)
+        while(sum >= happiness_value && day_count < d)
         {
             sum /= 2;
             day_count++;
         }
     }
 
-    return day_count - 1 <= d;
+    return sum >= happiness_value;
 }
 
 int binary_search(int l, int r)
 {
     if(l == r)
         return l;
+
+    if(r - l == 1)
+        return (pos(l) ? l : r);
     int mid = (l + r) / 2;
 
-    if(pos(mid))
+    if(!pos(mid))
         return binary_search(l, mid);
 
-    return binary_search(mid + 1, r);
+    return binary_search(mid, r);
 }
 
 int main()
